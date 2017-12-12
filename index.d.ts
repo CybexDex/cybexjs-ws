dd70fdcf36f669cc8f49b5f258c62421984aa9f2
ldwformat <ldwformat@gmail.com>
2017-12-12 12:30:33 +0800

    fix index.d

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module "cybexjs-ws" {
   class Apis {
     static setRpcConnectionStatusCallback(handler: (status: any) => any): void;
-    static instance(cs: string, connect: boolean, connectTimeout?: number, enableCrypto?: boolean): Apis;
+    static instance(cs?: string, connect?: boolean, connectTimeout?: number, enableCrypto?: boolean): Apis;
     init_promise: Promise<any>
   }
   const ChainConfig: any;
