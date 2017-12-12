b90eb616bd3640e2f8267c2dc3d9d9ccd036aec9
ldwformat <ldwformat@gmail.com>
2017-12-12 12:44:27 +0800

    fix index.d

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,25 @@
 declare module "cybexjs-ws" {
-  class Apis {
-    static setRpcConnectionStatusCallback(handler: (status: any) => any): void;
-    static instance(cs?: string, connect?: boolean, connectTimeout?: number, enableCrypto?: boolean): Apis;
+  class GrapheneApi {
+
+  }
+
+  class ApisInstance {
+    connect(): void;
+    close(): void;
+    db_api(): GrapheneApi;
+    history_api(): GrapheneApi;
+    network_api(): GrapheneApi;
+    crypto_api(): GrapheneApi;
+    setRpcConnectionStatusCallback(handler: (status: any) => any): void;
+  }
+  const Apis: {
+    setRpcConnectionStatusCallback(handler: (status: any) => any): void;
+    instance(cs?: string, connect?: boolean, connectTimeout?: number, enableCrypto?: boolean): ApisInstance;
     init_promise: Promise<any>
+    close: Promise<any>
+    reset: Promise<any>
+    setAutoReconnect(auto: boolean): void;
+    chainId(): string;
   }
   const ChainConfig: any;
 }
